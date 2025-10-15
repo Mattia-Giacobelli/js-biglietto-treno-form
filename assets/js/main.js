98621913db2453ae7cb35f88d70bfe00a76700fc
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-10-15 16:48:46 +0200

    create form and change default behavior

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,15 +8,27 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del bigliett
 il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
-*/
 
-/*
 MILESTONE 1:
 Iniziamo implementando il programma senza alcuna estetica: 
 usando esclusivamente due input e un bottone (non stilizzati), 
 realizziamo le specifiche scritte sopra. La risposta finale (o output) 
 sarà anch’essa da scrivere in console. 
+
+MILESTONE 2:
+Solo una volta che il milestone 1 sarà completo e funzionante allora 
+realizzeremo un form in pagina in cui l’utente potrà inserire i dati e 
+visualizzare il calcolo finale con il prezzo. 
+Il recap dei dati e l'output del prezzo finale, andranno quindi stampati 
+in pagina (il prezzo dovrà essere formattato con massimo due decimali, 
+per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+MILESTONE 3:
+Ora che la logica è funzionante in pagina, possiamo andare a dedicarci allo stile,
+raffinando la parte di HTML e CSS in modo da renderla esteticamente gradevole.
+
 */
+
 
 //Save ticket price, inputs and button in variables
 
@@ -25,13 +37,15 @@ const pricePerKm = 0.21
 const ageInput = document.getElementById('age')
 const kilometersInput = document.getElementById('kilometers')
 const buttonEl = document.querySelector('button')
+const form = document.querySelector('form')
 
 //Save user info in variables
 
 let age = 0;
 let kilometers = 0
 
-buttonEl.addEventListener('click', () => {
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
     age = ageInput.value
     kilometers = kilometersInput.value
     console.log(age, kilometers);
@@ -53,11 +67,11 @@ buttonEl.addEventListener('click', () => {
         finalPrice = basePrice
     }
 
+    //Stamp calc result
     console.log(finalPrice.toFixed(2));
 }
-    //Stamp calc result
-
 )
+
 
 
 
