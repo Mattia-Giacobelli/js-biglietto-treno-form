9244ce2ab35c33747a8133a91ffdd3a080c87b3e
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-10-15 15:50:53 +0200

    pseudo code

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+console.log('It works');
+
+/*
+Scrivere un programma che chieda all’utente:
+Il numero di chilometri da percorrere
+Età del passeggero
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+*/
+
+//Save ticket price and user info in variables
+
+//Create a function to calc the ticket cost
+
+//Multiply kilometers by price/km
+
+//Apply discount based on age
+
+//Stamp calc result
